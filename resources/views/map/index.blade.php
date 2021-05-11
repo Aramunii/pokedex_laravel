6c23bb5b5df27e5fd5789fd48a01858964ac830b
Warley Maer <Warley.maer@gmail.com>
2021-05-11 18:01:44 -0300

    Mapa

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -1,4 +1,3 @@
-
 @section('content')
     @if (!Auth::guest())
 
@@ -15,36 +14,66 @@
             </div>
             <div class="panel-body">
                 <div class="row text-center">
-                    <h4 id="name-poke">KKKKK</h4>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <a id="hunt-poke" class="btn btn-block-group bg-teal-400"> Caçar pokémon </a>
+                    <div class="container-fluid" style="
+                            background-image: URL(https://d.furaffinity.net/art/blazingifrit/1475361156/1475361156.blazingifrit_zoruaeevee_background.jpg);
+                            width: 900px;
+                            height: 582px;
+                        ">
+                        <div class="row" style="height: 50%">
+                        </div>
+                        <div class="row" id="pokemon" style="height: 30%">
+                        <!-- Aqui é a Row onde vai estar os pokemons que vão aparecer. -->
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 
-<script>
+    <script>
 
-    $(function (){
+        $(function () {
 
-        $('#hunt-poke').on('click',function (){
-            var number = Math.floor(Math.random() * 1000);
-            var url = 'https://pokeapi.co/api/v2/pokemon/' + number;
-                     fetch(url, {
-                "body": null,
-                "method": "GET",
-                "mode": "cors",
-                "credentials": "include"
-            }).then(function(response) {
-               console.log(response.json());
-            });
+
+            for(i=0;i<4; i++)
+            {
+                var number = Math.floor(Math.random() * 700);
+                var url = 'https://pokeapi.co/api/v2/pokemon/' + number;
+
+                /* Utilizando a função $.ajax({OPTIONS}) a gente passa os parametros
+                    url : a url que você quer fazer o request
+                    method : GET ou POST
+                    dataType: qual o tipo de resposta que vai ter, geralmente api é json;
+                    success : é executado uma função quando for concluido ( podendo pegar as informações )
+
+                 */
+
+                $.ajax
+                ({
+                    url: url,
+                    method: 'get',
+                    dataType: 'json',
+                    success: function (data) {
+
+                        //Aqui você cria o 'container' de cada pokemon, montando um html básico.
+                        var poke = `<div class='col-md-3'>
+                                    <img popup='tooltip' title='${data.name}' src='${data.sprites.front_default}'>
+                                    </div>`
+
+                        //Utilizando a função append , você vai pendurar o html que criou anteriormente no elemento que você definiu, no caso a row de id Pokemon.
+                        $('#pokemon').append(poke);
+
+                    }
+                });
+            }
+
+
+
+
+
         })
-
-    })
-</script>
+    </script>
 
 @endsection
 
